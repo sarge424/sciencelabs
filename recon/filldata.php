89ef92b6f10629b82fe47e1970770f881cd3e751
Arthur Dent <46473231+sarge424@users.noreply.github.com>
2019-12-24 10:33:52 +0530

    recon finish

--- recon/filldata.php
+++ recon/filldata.php
@@ -9,7 +9,13 @@ $data = "";
 $var = 0;
 while ($row = $result->fetch_assoc()) {
     $date1 = $row['recon'];
-    $date2 = date('y') . " " . date('m') . " " . date('d');
+    $time = strtotime($date1);
+    $date1 = date('Y-m-d', $time);
+    $date2 = date("Y-m-d");
+
+    $date1 = DateTime::createFromFormat("Y-m-d", $date1);
+    $date2 = DateTime::createFromFormat("Y-m-d", $date2);
+
     $diff = date_diff($date1, $date2);
     $diff = (int) $diff->format("%a");
 
