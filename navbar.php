7fb97b458467b6cb81f3189d02c80eef1639a9bd
Arthur Dent <46473231+sarge424@users.noreply.github.com>
2019-12-28 16:08:12 +0530

    Update navbar.php
    added icons and fixed center button border

--- navbar.php
+++ navbar.php
@@ -24,12 +24,16 @@ if ($lev == 0) {
 }
 
 $lab = $_SESSION['lab'];
+$img = '';
 if ($lab == 'b') {
 	$lab = 'success';
+	$img = '<img src="../img/cells.svg" width="30" height="30">';
 } else if ($lab == 'c') {
 	$lab = 'danger';
+	$img = '<img src="../img/flask.svg" width="30" height="30">';
 } else {
 	$lab = 'primary';
+	$img = '<img src="../img/atom.svg" width="30" height="30">';
 }
 ?>
 
@@ -78,12 +82,18 @@ if ($lab == 'b') {
 
 	<div class="mx-auto order-0">
 		<div class="dropdown">
-			<button class="btn btn-dark navbar-brand dropdown-toggle" style="background:rgba(41, 43, 44, 0.5);" type="button" data-toggle="dropdown"><?php echo $_SESSION['labname']; ?>Lab
+			<button class="btn btn-dark navbar-brand dropdown-toggle" style="background:rgba(41, 43, 44, 0.5);border:none;" type="button" data-toggle="dropdown"><?php echo $img.' '.$_SESSION['labname']; ?>Lab
 				<span class="caret"></span></button>
 			<ul class="dropdown-menu">
-				<li><a class="dropdown-item" onclick="document.location.href='../changelab.php?labname=p';">Physics Lab</a></li>
-				<li><a class="dropdown-item" onclick="document.location.href='../changelab.php?labname=c';">Chemistry Lab</a></li>
-				<li><a class="dropdown-item" onclick="document.location.href='../changelab.php?labname=b';">Biology Lab</a></li>
+				<li><a class="dropdown-item" onclick="document.location.href='../changelab.php?labname=p';">
+					<img src="../img/atom.svg" width="30" height="30"> Physics Lab
+				</a></li>
+				<li><a class="dropdown-item" onclick="document.location.href='../changelab.php?labname=c';">
+					<img src="../img/flask.svg" width="30" height="30"> Chemistry Lab
+				</a></li>
+				<li><a class="dropdown-item" onclick="document.location.href='../changelab.php?labname=b';">
+					<img src="../img/cells.svg" width="30" height="30"> Biology Lab
+				</a></li>
 			</ul>
 		</div>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
