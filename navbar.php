d9de61e84f0f58b6710a3a98d94ef55abb1c2004
Arthur Dent <46473231+sarge424@users.noreply.github.com>
2019-12-30 14:06:01 +0530

    Bugs

--- navbar.php
+++ navbar.php
@@ -82,18 +82,18 @@ if ($lab == 'b') {
 
 	<div class="mx-auto order-0">
 		<div class="dropdown">
-			<button class="btn btn-dark navbar-brand dropdown-toggle" style="background:rgba(41, 43, 44, 0.5);border:none;" type="button" data-toggle="dropdown"><?php echo $img.' '.$_SESSION['labname']; ?>Lab
+			<button class="btn btn-dark navbar-brand dropdown-toggle" style="background:rgba(41, 43, 44, 0.5);border:none;" type="button" data-toggle="dropdown"><?php echo $img . ' ' . $_SESSION['labname']; ?>Lab
 				<span class="caret"></span></button>
 			<ul class="dropdown-menu">
 				<li><a class="dropdown-item" onclick="document.location.href='../changelab.php?labname=p';">
-					<img src="../img/atom.svg" width="30" height="30"> Physics Lab
-				</a></li>
+						<img src="../img/atom.svg" width="30" height="30"> Physics Lab
+					</a></li>
 				<li><a class="dropdown-item" onclick="document.location.href='../changelab.php?labname=c';">
-					<img src="../img/flask.svg" width="30" height="30"> Chemistry Lab
-				</a></li>
+						<img src="../img/flask.svg" width="30" height="30"> Chemistry Lab
+					</a></li>
 				<li><a class="dropdown-item" onclick="document.location.href='../changelab.php?labname=b';">
-					<img src="../img/cells.svg" width="30" height="30"> Biology Lab
-				</a></li>
+						<img src="../img/cells.svg" width="30" height="30"> Biology Lab
+					</a></li>
 			</ul>
 		</div>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
@@ -103,17 +103,22 @@ if ($lab == 'b') {
 
 	<div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
 		<ul class="navbar-nav ml-auto">
+			<span class="navbar-text text-white">
+				<?php echo $level . $uname; ?>
+			</span>
+			<?php
+			if ($_SESSION['level'] != 2) {
+				echo '<li class="nav-item"><a class="nav-link" href="/sciencelabs/labtransactions">Lab Transfers</a>';
+			}
+			?>
+			<?php
+			if ($_SESSION['level'] != 2) {
+				echo '<li class="nav-item"><a class="nav-link" href="../reviewrequest/">Review Pending Orders</a>';
+			}
+			?>
 			<li class="nav-item dropdown ml-auto">
-				<span class="navbar-text text-white">
-					<?php echo $level . $uname; ?>
-				</span>
 				<a class="nav-link dropdown-toggle navbar-text text-white" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="fa fa-gear fa-spin" style="font-size:20px"></i> </a>
 				<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-					<?php
-					if ($_SESSION['level'] != 2) {
-						echo '<a class="dropdown-item" href="../reviewrequest/">Review Pending Orders</a><div class="dropdown-divider"></div>';
-					}
-					?>
 					<a class="dropdown-item" href="../user/changepass.php">Change Password</a>
 					<a class="dropdown-item" href="../">Logout</a>
 				</div>
