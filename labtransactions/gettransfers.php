2b4bf76f24cb961e9716a25b62aeaefffccc3efd
Arthur Dent <46473231+sarge424@users.noreply.github.com>
2019-12-30 14:29:01 +0530

    Prev Transfers done

---
+++ labtransactions/gettransfers.php
@@ -0,0 +1,28 @@
+<?php
+require_once '../db.php';
+require_once '../checksession.php';
+
+$sql = "select * from dept_transaction where from_lab='" . $_SESSION['lab'] . "';";
+$result = $conn->query($sql);
+
+$data = "";
+
+if ($result->num_rows != 0) {
+    while ($row = $result->fetch_assoc()) {
+        $sql = "select item_name from item where id=" . $row['item_id'] . ";";
+        $item = $conn->query($sql);
+        $item_name = "";
+        while ($id = $item->fetch_assoc()) {
+            $item_name = $id['item_name'];
+        }
+
+        $data = $data .
+            '<tr>' .
+            '<td>' . strtoupper($row['from_lab']) .
+            '<td>' . strtoupper($row['to_lab']) .
+            '<td>' . $item_name .
+            '<td>' . $row['quantity'] .
+            '<td>' . $row['transfer_date'];
+    }
+}
+echo $data;
