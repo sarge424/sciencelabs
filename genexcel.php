f5eb7fbac5de442eef8339499b6fcf5cc5a5cc65
Arthur Dent <46473231+sarge424@users.noreply.github.com>
2020-01-03 16:44:30 +0530

    excel done

---
+++ genexcel.php
@@ -0,0 +1,46 @@
+<?php
+require_once 'db.php';
+require_once 'checksession.php';
+require_once 'PHPSpreadsheet/vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+$spreadsheet = new Spreadsheet();
+$sheet = $spreadsheet->getActiveSheet();
+$sheet->setCellValue('A1', 'Item Name');
+$sheet->setCellValue('B1', 'Specifications');
+$sheet->setCellValue('C1', 'Quantity');
+$sheet->setCellValue('D1', 'Link');
+$sheet->setCellValue('E1', 'Approx. Cost');
+
+$sheet->getStyle("A1:E1")->getFont()->setBold(true);
+
+$sql = "select item_name, specs, quantity_ordered, link, cost from purchase_request where arrived=0;";
+$result = $conn->query($sql);
+
+$row = "A";
+$col = "2";
+
+if ($result->num_rows != 0) {
+    while ($info = $result->fetch_row()) {
+        $sheet->setCellValue($row . $col, $info[0]);
+        $row = chr(ord($row) + 1);
+        $sheet->setCellValue($row . $col, $info[1]);
+        $row = chr(ord($row) + 1);
+        $sheet->setCellValue($row . $col, $info[2]);
+        $row = chr(ord($row) + 1);
+        $sheet->setCellValue($row . $col, $info[3]);
+        $row = chr(ord($row) + 1);
+        $sheet->setCellValue($row . $col, $info[4]);
+
+        $row = "A";
+        $col = (string) ((int) $col + 1);
+    }
+}
+
+$writer = new Xlsx($spreadsheet);
+$writer->save(date("Y m d") . ' Purchase.xlsx');
+
+header("Location: /sciencelabs/home/");
+exit;
