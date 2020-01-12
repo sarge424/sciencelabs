e586da491dc991467324f08c39bbf107afe5b59c
Mark Dayton <46218216+papaya147@users.noreply.github.com>
2020-01-12 11:01:31 +0530

    fixed experiment partial dep.
    expedit is still buggy tho

--- itembooking/submitexp.php
+++ itembooking/submitexp.php
@@ -6,19 +6,29 @@ $id = $_GET['id'];
 $q = $_GET['q'];
 $name = $_GET['name'];
 $exists = false;
+$expid = 0;
 
-$sql = 'select distinct date_format(date_created, "%Y-%m-%d %H:%i:%s") as date from experiment where exp_name="' . $name . '";';
+$sql = 'select id, date_format(date_created, "%Y-%m-%d %H:%i:%s") as date from experiment where exp_name="' . $name . '";';
 $result = $conn->query($sql);
 
-while($row = $result->fetch_assoc()) {
-    $datetime2 = new DateTime();
-    $datetime1 = new DateTime($row['date']);
-    $interval = $datetime1->getTimestamp() - $datetime2->getTimestamp();
-    if(abs($interval) > 60)
-        $exists = true;
+if($result->num_rows > 0){
+    while($row = $result->fetch_assoc()) {
+        $expid = $row['id'];
+        $datetime2 = new DateTime();
+        $datetime1 = new DateTime($row['date']);
+        $interval = $datetime1->getTimestamp() - $datetime2->getTimestamp();
+        echo $interval;
+        if(abs($interval) <= 20000)
+            $exists = true;
+    }
+}else{
+    $sql = 'insert into experiment(exp_name) values ("'.$name.'");';
+    $conn->query($sql);
+    $sql = 'select id from experiment where exp_name = "'.$name.'";';
+    $expid = $conn->query($sql)->fetch_assoc()['id'];
 }
 
-$sql = 'INSERT INTO experiment (exp_name, item_id, quantity) VALUES ("' . $name . '", ' . $id . ', ' . $q . ');';
+$sql = 'INSERT INTO experiment_item (exp_id, item_id, quantity) VALUES ("' . $expid . '", ' . $id . ', ' . $q . ');';
 if (!$exists) {
     $conn->query($sql);
     echo 'done';
