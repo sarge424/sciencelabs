4ac8fe39d97f741d62c4885d63687aca1bd0f2b0
Arthur Dent <46473231+sarge424@users.noreply.github.com>
2020-02-27 12:02:07 +0530

    removing stuff for final version
    whee

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
-$servername = 'localhost';
-$username = 'username';
-$password = 'password';
+$servername = '10.0.0.7';
+$username = 'labs_user';
+$password = 'hello_world';
 $dbname = 'labs';
 
 $conn = new mysqli($servername, $username, $password, $dbname);
