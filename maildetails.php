7549ef5b9212b88e2fa00da45c3d7252fd7c6aba
Arthur Dent <46473231+sarge424@users.noreply.github.com>
2020-02-27 12:19:48 +0530

    Edited maildetails

--- maildetails.php
+++ maildetails.php
@@ -7,9 +7,11 @@ $mail->SMTPAuth = true; // enable SMTP authentication
 $mail->SMTPSecure = "ssl"; // sets the prefix to the server
 $mail->Host = "smtp.gmail.com"; // sets GMAIL as the SMTP server
 $mail->Port = 465; // set the SMTP port for the GMAIL server
-$mail->Username = "abhinav@ishahomeschool.org"; // GMAIL username
-$mail->Password = "pass"; // GMAIL password
+
+//------SET TO THE APPROPRIATE EMAIL ID------
+$mail->Username = "random.it.email@ishahomeschool.org"; // GMAIL username
+$mail->Password = "password"; // GMAIL password
 
 //Senders information
-$email_from = "abhinav@ishahomeschool";
-$name_from = "Abhinav Srivatsa";
+$email_from = "random.it.email@ishahomeschool.org";
+$name_from = "IT robot";
